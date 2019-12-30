75fa36cb00baadaf106263b31b49a75d919149ff
Percy <percival.ngan@gmail.com>
2019-12-30 17:53:11 -0600

    implement cellForRowAt

--- Reading-List/Reading List/View Controllers/ReadingListTableViewController.swift
+++ Reading-List/Reading List/View Controllers/ReadingListTableViewController.swift
@@ -54,11 +54,13 @@ class ReadingListTableViewController: UITableViewController, BookTableViewCellDe
 
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath)
 
-        // Configure the cell...
+		guard let bookCell = cell as? BookTableViewCell else { return cell }
+		bookCell.delegate = self
+		bookCell.book = bookFor(indexpath: indexPath)
 
-        return cell
+		return bookCell
     }
 
 
@@ -70,17 +72,13 @@ class ReadingListTableViewController: UITableViewController, BookTableViewCellDe
     }
     */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
